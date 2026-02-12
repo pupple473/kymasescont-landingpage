47527f8029ea770a22401af0fbfe85fca9783e90
Oswaldo Sevillano <oswaldosevillano@MacBook-Air-de-Oswaldo.local>
2026-02-12 18:28:08 -0300

    Second commit. Initialization of build to Hero component and some mor visual n funtional caracteristics

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,9 @@
+import { ChevronDown } from "lucide-react";
 import {useEffect, useState} from "react"; 
 export default function Hero() { 
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+    const [activeTab, setActiveTab] = useState({ x: 0, y: 0 });
+
     useEffect(() => {
         function handleMouseMove(e){
             setMousePosition({ x: e.clientX, y: e.clientY });
@@ -23,11 +26,50 @@ export default function Hero() {
             }/>
             <div className="absolute top-20 left-4 sm:left-10. w-48 sm:w-72. h-48 sm:h-72 bg-blue-500/10 rounded-full blur-3xl animate-pulse"/>
             <div className="absolute buttom-20 right-4 sm:right-10 w-64 sm:w-96 h-64 sm:h-96 bg-cyan-500/10 rounded-full blur-3xl animate-pulse delay-1000"/>
-            <div>
-                <div>
-                    <div>
-                        
+            <div className="relative order-2 w-full">
+                <div className="relative bg-white/5 backdrop-blur-xl rounded-xl sm:rounded-2xl p-3 sm:p-4 shadow-2xl border border-white/10">
+                    <div className="bg-gradient-to-br from-gray-900/20 to-gray-800/20 backdrop-blur-sm rounded-lg overflow-hidden h-[280px] sm:w-[350px] lg:h-[450px] border border-white/5 ">
+                        <div className="flex items-center justify-between px-3 sm:px-4 py-2 sm:py-3 bg-white/5 backdrop-blur-sm border-b border-white/10 "> 
+                            <div className="flex items-center space-x-2">
+                                <div className="flex items-center space-x-1 sm:space-x-2">
+                                    <div className="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-red-500 "/>
+                                    <div className="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-yellow-500 "/>
+                                    <div className="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-green-500 "/>
+                                </div>
+                                <span className="text-xs sm:text-sm text-gray-300">Asescont KyM</span>
+                            </div>
+                        <ChevronDown className="w-3 h-3 sm:w-4 sm:h-4 text-gray-400 "/>
+
+                        </div>
+                        <div className="p-3 sm:p-4 relative h-full">
+                            {/*files tabs*/}
+                            <div className="flex space-x-1 sm:space-x-2 mb-3 sm:mb-4 overflow-x-auto">
+                                <button 
+                                        onClick={ () => setActiveTab("Ventana 1")}
+                                    className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border ${activeTab === "Ventana 1" 
+                                        ? "bg-blue500/30 text-white border-blue-400/20" 
+                                        : "bg-white-5 text-gray-300 border-white-10 hover:bg-white-10"}  transition-all duration-200 whitespace-nowrap`}>
+                                            Ventana 1
+                                </button>
+                                <button
+                                    onClick={ () => setActiveTab("Ventana 2")}
+                                    className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border ${activeTab === "Ventana 2" 
+                                        ? "bg-blue500/30 text-white border-blue-400/20" 
+                                        : "bg-white-5 text-gray-300 border-white-10 hover:bg-white-10"}  transition-all duration-200 whitespace-nowrap`}>
+                                            Ventana 2
+                                </button>                                
+                                <button 
+                                    onClick={ () => setActiveTab("Ventana 3")}
+                                    className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border ${activeTab === "Ventana 3" 
+                                        ? "bg-blue500/30 text-white border-blue-400/20" 
+                                        : "bg-white-5 text-gray-300 border-white-10 hover:bg-white-10"}  transition-all duration-200 whitespace-nowrap`}>
+                                            Ventana 3
+                                </button>
+                            </div>
+                            {/*code content*/}
+                        </div>
                     </div>
+
                 </div>
             </div>
         </section>
---
+++ src/data/CodeExamples.js
@@ -0,0 +1,16 @@
+
+
+export const codeExamples = {
+    "Ventana 1": `
+        export const saludo = "Hola, mundo!";
+        return(console.log(saludo));
+    `,
+    "Ventana 2": `  
+        const saludo = "Hola, mundo!";
+        console.log(saludo);
+    `,
+    "Ventana 3": `
+        const saludo = "Hola, mundo!";
+        console.log(saludo);
+    `,
+}
